5973c0e203e5de096e2891362714489fef86fd4e
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-03-26 13:39:25 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Loading } from "./components/common";
 import { Navbar, Footer } from "./components";
 import { Homepage } from "./pages";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    // console.log(location);
+  }, [location]);
+
+  setTimeout(() => setLoading(false), 3900);
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-      <Footer />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
